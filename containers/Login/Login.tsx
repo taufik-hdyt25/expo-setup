5346bb3b47f520088a961484f2ed91df8df3fc5a
taufik-hdyt25 <taufikh2519@gmail.com>
2025-11-21 16:36:05 +0700

    gix

--- containers/Login/Login.tsx
+++ containers/Login/Login.tsx
@@ -1,7 +1,15 @@
 import { saveToken } from "@/utils/secureStore";
 import { useFormik } from "formik";
 import React from "react";
-import { Dimensions, Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { TextInput } from "react-native-paper";
 
 const { height, width } = Dimensions.get("window");
@@ -17,53 +25,55 @@ const Login = () => {
       await saveToken("token", "ini_tokennyaaaaa");
     },
   });
-  return (
-    <View
-      style={{
-        height: height,
-        paddingHorizontal: 20,
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ fontSize: 40 }}>LOGIN</Text>
 
-      <View style={{ gap: 10, marginTop: 20 }}>
-        <View>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ height }}
+    >
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 20,
+          justifyContent: "center",
+        }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Text style={{ fontSize: 40 }}>LOGIN</Text>
+
+        <View style={{ gap: 10, marginTop: 20 }}>
           <TextInput
             mode="outlined"
             label="Email"
             placeholder="Input email"
             value={formik.values.email}
             onChangeText={formik.handleChange("email")}
-            // right={<TextInput.Affix text="/100" />}
           />
-        </View>
-        <View>
+
           <TextInput
             mode="outlined"
             label="Password"
             placeholder="Input password"
+            secureTextEntry
             value={formik.values.password}
             onChangeText={formik.handleChange("password")}
-            // right={<TextInput.Affix text="/100" />}
           />
         </View>
-      </View>
 
-      <TouchableOpacity
-        onPress={() => formik.handleSubmit()}
-        style={{
-          backgroundColor: "blue",
-          paddingVertical: 20,
-          paddingHorizontal: 10,
-          alignItems: "center",
-          borderRadius: 10,
-          marginTop: 20,
-        }}
-      >
-        <Text style={{ fontSize: 14, color: "white" }}>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity
+          onPress={() => formik.handleSubmit()}
+          style={{
+            backgroundColor: "blue",
+            paddingVertical: 20,
+            alignItems: "center",
+            borderRadius: 10,
+            marginTop: 20,
+          }}
+        >
+          <Text style={{ fontSize: 14, color: "white" }}>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
